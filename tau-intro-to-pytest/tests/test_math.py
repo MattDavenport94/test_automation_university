ecf4cac967c38abd9f215cb9cf7cf2dd6a69771a
Matthew Davenport <madavenport94@gmail.com>
2021-05-26 19:43:32 -0400

    adding pytest learning module

---
+++ tau-intro-to-pytest/tests/test_math.py
@@ -0,0 +1,7 @@
+"""
+This module contains basic unit tests for math operations.
+Their purpose is to show how to use the pytest framework for reference.
+"""
+
+def test_one_plus_one():
+    assert 1 + 1 == 2
