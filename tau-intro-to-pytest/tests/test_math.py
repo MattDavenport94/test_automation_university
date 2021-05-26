6c9bee5e6a900f4e2e9bba00c5e6e940a9e763fd
Matthew Davenport <madavenport94@gmail.com>
2021-05-26 19:57:02 -0400

    adding chapter 2 learning notes

--- tau-intro-to-pytest/tests/test_math.py
+++ tau-intro-to-pytest/tests/test_math.py
@@ -3,5 +3,18 @@ This module contains basic unit tests for math operations.
 Their purpose is to show how to use the pytest framework for reference.
 """
 
+#--------------------------------------------------------------------------
+# A basic test function
+#--------------------------------------------------------------------------
 def test_one_plus_one():
     assert 1 + 1 == 2
+
+#--------------------------------------------------------------------------
+# A test function to show assertion introspection
+#--------------------------------------------------------------------------
+
+def test_one_plus_two():
+    a = 1
+    b = 2
+    c = 3
+    assert a + b == c
